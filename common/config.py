f206893c18d3aca2263e570cb7ad7e5ebabfb13d
franbrizuelab <fran3.14159265@gmail.com>
2025-12-18 04:55:29 +0800

    Config done!

--- common/config.py
+++ common/config.py
@@ -1,12 +1,12 @@
 # Lobby Server
-LOBBY_HOST = '127.0.0.1'
-# LOBBY_HOST = '140.113.17.12'
+#LOBBY_HOST = '127.0.0.1'
+LOBBY_HOST = '140.113.17.12'
 LOBBY_PORT = 12690
 
 # DB Server
 # The Lobby will connect to this address.
-DB_HOST = '127.0.0.1'
-#DB_HOST = '140.113.17.12'
+#DB_HOST = '127.0.0.1'
+DB_HOST = '140.113.17.11'
 DB_PORT = 12069
 
 # Game Server
